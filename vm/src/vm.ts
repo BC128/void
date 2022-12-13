973eacf129984825c5ca0aee0f4a2d32505b22ac
SithLor <84148006+SithLor@users.noreply.github.com>
2022-12-13 22:07:30 +0000

    d

---
+++ vm/src/vm.ts
@@ -0,0 +1,8 @@
+
+type dogo_Type_String = string
+type dogo_Type_Hex = string
+type dogo_Type_Number = number
+type dogo_Type_Null = null
+type dogo_Type_boolen = boolean
+type dogo_Type_Undefined= undefined
+type dogo_Type_Void= void
