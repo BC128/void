580be94d839c3a2d049c522fa992ea902f1997d0
SithLor <84148006+SithLor@users.noreply.github.com>
2022-12-16 18:34:55 +0000

    d

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ class _Registry {
  * @param {string} key - The key to store the data under.
  * @param {string} data - the data to be stored
  */
-  public set(key:string,data:string): void{
+  public setItem(key:string,data:string): void{
     localStorage.setItem(key,data)
   }
 /**
@@ -13,7 +13,7 @@ class _Registry {
  * @param {string} key - The key of the item you want to get from localStorage.
  * @returns The value of the key in localStorage.
  */
-  public get(key:string): string | null{
+  public getItem(key:string): string | null{
     return localStorage.getItem(key)
   }
 /**
@@ -39,20 +39,40 @@ class _Registry {
     return localStorage.removeItem(key)
   }
 }
-const _registry = new _Registry()
-
-class _Boot {
-  public setRegistry(){
-      _registry.set("boot_time",`${Date.now()}`)
-      _registry.set("boot_lele",`${Date.now()}`)
-
+class _RegistryFS {
+  public removeItemFile(loc:string | URL){
+    Deno.remove(`./src/db/reg/${loc}`)
+  }
+  public async getItemFile(loc:string | URL){
+    const decoder = new TextDecoder("utf-8");
+    const data = await Deno.readFile(`./src/db/reg/${loc}`);
+    return decoder.decode(data)
+  }
+  public async setItemFile(loc:string | URL,data_:string){
+    const encoder = new TextEncoder();
+    const data = encoder.encode(data_+"\n");
+    const file = await Deno.create(`./src/db/reg/${loc}`)
+    await Deno.writeFile(`./src/db/reg/${loc}`, data);
   }
 }
+class _Boot {
+  public setRegistry(){
+      //_registry.setItem("boot/uptime.reg",`${Date.now()}`)
+      //_registry.setItem("boot/verions/pkg.reg",`0.0.1`)
+      //_registry.setItem("boot/verions/kernel.reg","0.0.1")
+      _registryFS.setItemFile("boot/test.reg",`${Date.now()}`)
+      _registryFS.setItemFile("boot/verions/pkg.reg",`0.0.1`)
+      _registryFS.setItemFile("boot/verions/kernel.reg","0.0.1")
+  }
+}
+const _registry = new _Registry()
 const _boot = new _Boot()
+const _registryFS = new _RegistryFS()
 const tsLixux = {
   registry:_registry,
+  registryFS:_registryFS,
   boot:_boot
 }
 
 tsLixux.boot.setRegistry()
-console.log(tsLixux.registry.get("boot_time")?.toString())
+console.log(tsLixux.registryFS.getItemFile("boot/test.reg"))
