0745d4641587c59b696466ed2d2d11feb86d63bf
SithLor <84148006+SithLor@users.noreply.github.com>
2022-12-16 21:24:41 +0000

    j

--- src/main.ts
+++ src/main.ts
@@ -39,36 +39,15 @@ class _Registry {
     return localStorage.removeItem(key)
   }
 }
-class _RegistryFS {
-  public removeItemFile(loc:string | URL){
-    Deno.remove(`./src/db/reg/${loc}`)
-  }
-  public async getItemFile(loc:string){
-    const decoder = new TextDecoder();
-    const data = Deno.readFileSync(("./src/db/reg/"+loc);
-
-    return decoder.
-  }
-  public async setItemFile(loc:string | URL,data_:string){
-    const encoder = new TextEncoder();
-    const data = encoder.encode(data_+"\n");
-    const file = Deno.create(`./src/db/reg/${loc}`)
-    await Deno.writeFile(`./src/db/reg/${loc}`, data);
-  }
-  public async setItemGroup(loc:string | URL){
-    await Deno.mkdir(`./src/db/reg/${loc}`,{recursive: true})
-  }
-}
+class turtlefs
 class _Boot {
   public setRegistry(){
       //_registry.setItem("boot/uptime.reg",`${Date.now()}`)
       //_registry.setItem("boot/verions/pkg.reg",`0.0.1`)
       //_registry.setItem("boot/verions/kernel.reg","0.0.1")
-      _registryFS.setItemGroup("boot")
-      _registryFS.setItemGroup("boot/verions")
-      _registryFS.setItemFile("boot/uptime.reg",`${Date.now()}`)
-      _registryFS.setItemFile("boot/verions/pkg.reg",`0.0.1`)
-      _registryFS.setItemFile("boot/verions/kernel.reg","0.0.1")
+      _registry.setItem("boot/uptime.reg",`${Date.now()}`)
+      _registry.setItem("boot/verions/pkg.reg",`0.0.1`)
+      _registry.setItem("boot/verions/kernel.reg","0.0.1")
   }
 }
 const _registry = new _Registry()
@@ -76,9 +55,11 @@ const _boot = new _Boot()
 const _registryFS = new _RegistryFS()
 const tsLixux = {
   registry:_registry,
-  registryFS:_registryFS,
-  boot:_boot
+  //registry:_registry,
+  //FS:FS,
+  boot:_boot,
 }
 
 tsLixux.boot.setRegistry()
-console.log(tsLixux.registryFS.getItemFile("boot/test.reg"))
+console.log(tsLixux.registry.getItem("boot/uptime.reg"))
+sessionStorage.
